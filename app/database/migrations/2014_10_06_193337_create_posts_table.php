7af849793aa58127faf48ccf92876d608712e9dc
Jbns89 <Jbns89@gmail.com>
2014-10-06 14:47:16 -0500

    Creating a database table for blog using laravel

---
+++ app/database/migrations/2014_10_06_193337_create_posts_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePostsTable extends Migration {
+
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('posts', function(Blueprint $table)
+        {
+            //creates auto incrementing id keys
+            $table->increments('id');
+            $table->timestamps();
+            $table->string('title', 255);
+            $table->text('content');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('posts');
+    }
+
+}
