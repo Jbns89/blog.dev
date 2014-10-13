295455975fb265202801eab2f26939123ad0667a
Jbns89 <Jbns89@gmail.com>
2014-10-13 15:15:58 -0500

    added way\generator and debug bar

--- app/config/local/app.php
+++ app/config/local/app.php
@@ -5,17 +5,23 @@
 
 return array(
 
-	/*
-	|--------------------------------------------------------------------------
-	| Application Debug Mode
-	|--------------------------------------------------------------------------
-	|
-	| When your application is in debug mode, detailed error messages with
-	| stack traces will be shown on every error that occurs within your
-	| application. If disabled, a simple generic error page is shown.
-	|
-	*/
+    /*
+    |--------------------------------------------------------------------------
+    | Application Debug Mode
+    |--------------------------------------------------------------------------
+    |
+    | When your application is in debug mode, detailed error messages with
+    | stack traces will be shown on every error that occurs within your
+    | application. If disabled, a simple generic error page is shown.
+    |
+    */
 
-	'debug' => true,
+    'debug' => true,
+    'provider' => array(
+        'Way\Generators\GeneratorsServiceProvider',
+        'Barryvdh\Debugbar\ServiceProvider'
+        )
 
 );
+
+
