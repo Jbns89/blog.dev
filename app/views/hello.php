47c68a6fc059f4d1ade9b3f5de1211e554b3581f
Jbns89 <Jbns89@gmail.com>
2015-06-14 14:22:17 -0500

    new layout

--- app/views/hello.php
+++ app/views/hello.php
@@ -1,14 +1,53 @@
 <!doctype html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <title>Jilly Beans</title>
-    <link rel="stylesheet" type="text/css" href="/css/hello.css">
-    <link href='http://fonts.googleapis.com/css?family=The+Girl+Next+Door' rel='stylesheet' type='text/css'>
-</head>
-<body>
-	<div class="welcome">
-		<h1>You have arrived.  Sexy.</h1>
-	</div>
-</body>
+    <head>
+        <meta charset="UTF-8">
+        <title>Jilly Beans</title>
+        <link rel="stylesheet" type="text/css" href="/css/hello.css">
+        <link href="http://fonts.googleapis.com/css?family=The+Girl+Next+Door" rel="stylesheet" type="text/css">
+        <style type="text/css"></style>
+    </head>
+    <body>
+        <div id="navbar">
+            <div class="logo">
+                <a href="/"><img src="/img/jdlogo.png"></a>
+            </div>
+            <ul>
+                <li><a href="">About Me</a></li>
+                <li><a href="">Blog</a></li>
+                <li><a href="">Free Things</a></li>
+            </ul>
+        </div>
+        <div id="wrapper">
+            <div class="container">
+                <h1>You have arrived.  Sexy.</h1>
+                <div class="post">
+                    <h2>Insert Title</h2>
+                    <h4>Insert Date</h4>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                </div>
+                <div class="post">
+                    <h2>Insert Title</h2>
+                    <h4>Insert Date</h4>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                </div>
+                <div class="post">
+                    <h2>Insert Title</h2>
+                    <h4>Insert Date</h4>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                </div>
+                <div class="post">
+                    <h2>Insert Title</h2>
+                    <h4>Insert Date</h4>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                </div>
+                <div class="post">
+                    <h2>Insert Title</h2>
+                    <h4>Insert Date</h4>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                </div>
+            </div>
+        </div>
+    </body>
+    
 </html>
