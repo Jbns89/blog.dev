67bda26be6ad7889051e8848354c6c26df9e41a0
Jbns89 <Jbns89@gmail.com>
2015-09-11 18:16:47 -0500

    bobs burger invite file

---
+++ app/views/bb_invite.blade.php
@@ -0,0 +1,27 @@
+<!doctype html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <title>Bob's Burgers House Warming</title>
+        <link rel="stylesheet" type="text/css" href="/css/bb-invite.css">
+        <link href="http://fonts.googleapis.com/css?family=The+Girl+Next+Door" rel="stylesheet" type="text/css">
+        <script src="http://code.jquery.com/jquery-1.11.0.min.js"></script>
+        <script type="text/javascript">
+        $(document).ready(function(){
+            $("#bbtheme").trigger("play");
+        });
+        </script>
+    </head>
+    <body>
+        <main> 
+            <audio class="audio-player" id="bbtheme" src="/img/bbtheme.mp3"></audio>
+            <img class="main" src="/img/bb-bkg.png">
+            <img class="tina" src="/img/Tina_render.png">
+            <img class="bob" src="/img/bob.jpg">
+            <img class="gene" src="/img/Gene_Belcher.png">
+            <img class="linda" src="/img/Lindab.png">
+            <img class="louise" src="/img/louise_belcher.png">
+        </main>
+    </body>
+    
+</html>
